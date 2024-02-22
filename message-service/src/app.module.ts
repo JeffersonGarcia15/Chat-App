23bf8ab18b1284e3c8a1bcb36f99830c29d21b0a
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-02-22 17:59:49 -0600

    message-service-config-setup/jalg intializing config.ts, environments.ts and validating env variables with Joi.

--- message-service/src/app.module.ts
+++ message-service/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MessagesModule } from "./messages/services/messages/messages.module";
+import { environments } from "./environments";
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    MessagesModule,
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || ".env",
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod", "stag").default("dev"),
+        PORT: Joi.number().default(5432),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
