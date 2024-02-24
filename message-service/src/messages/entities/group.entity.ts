7c888f074a59463e0c105c2b1082139a4af87262
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-02-23 18:23:29 -0600

    message-service-entities-setup/jalg First draft for group.entity.ts as well as adding CreatedAt and UpdatedAt fields.

--- message-service/src/messages/entities/group.entity.ts
+++ message-service/src/messages/entities/group.entity.ts
@@ -7,27 +7,15 @@ import {
 } from "typeorm";
 
 @Entity()
-export class Message {
+export class Group {
   @PrimaryGeneratedColumn()
   Id: number;
 
-  @Column({ type: "text" })
-  Content: string;
+  @Column({ type: "string", length: 100 })
+  Name: string;
 
-  @Column({ type: "int" })
-  SenderId: number;
-
-  @Column({ type: "int", nullable: true })
-  ReceiverId: number;
-
-  @Column({ type: "int", nullable: true })
-  GroupId: number;
-
-  @Column({ type: "enum", enum: ["edited", "deleted"] })
-  Status: string;
-
-  @Column({ type: "enum", enum: ["text", "image", "video", "audio", "file"] })
-  Type: string;
+  @Column({ type: "string", length: 200, nullable: true })
+  Description: string;
 
   @CreateDateColumn({
     name: "created_at",
