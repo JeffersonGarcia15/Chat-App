aac2b512a34a8109819a83b1c02480d7cb2fafd9
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-03-07 14:54:10 -0600

    media-service-setup/jalg installing dependencies, modifying main and app.module to include kafka as well as adding a service and a controller for the media.

--- media-service/src/app.module.ts
+++ media-service/src/app.module.ts
@@ -1,10 +1,33 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { ConfigModule } from "@nestjs/config";
+import { MediaController } from "./controllers/media/media.controller";
+import { MediaService } from "./services/media/media.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "KAFKA_SERVICE",
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ["kafka:9092"],
+          },
+          consumer: {
+            groupId: "notification-service-group",
+          },
+        },
+      },
+    ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+  ],
+  controllers: [AppController, MediaController],
+  providers: [AppService, MediaService],
 })
 export class AppModule {}
