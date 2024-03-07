60dc30e02921a9941a324fd6f9fa0a78f8ab7c75
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-03-07 17:38:25 -0600

    media-service-config-setup/jalg Adding Joi for validation and setting up a config file in order to reduce the risk of typos.

--- media-service/src/app.module.ts
+++ media-service/src/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule } from "@nestjs/config";
 import { MediaController } from "./media/controllers/media/media.controller";
 import { MediaService } from "./media/services/media/media.service";
 import { MediaModule } from "./media/media.module";
+import { environments } from "./environments";
+import config from "./config";
+import * as Joi from "joi";
 
 @Module({
   imports: [
@@ -25,7 +28,15 @@ import { MediaModule } from "./media/media.module";
     ]),
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".env",
+      envFilePath: environments[process.env.NODE_ENV] || ".env",
+      load: [config],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod", "stag").default("dev"),
+        AWS_S3_REGION: Joi.string().required(),
+        AWS_S3_BUCKET: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+      }),
     }),
     MediaModule,
   ],
